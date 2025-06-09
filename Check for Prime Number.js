1fe76b6ec4ea21afed742af3dcaec21dd6df8fdd
Riyaz shaik <94755295+fayaz35341@users.noreply.github.com>
2025-06-09 20:06:01 +0530

    Update Check for Prime Number.js

--- Check for Prime Number.js
+++ Check for Prime Number.js
@@ -1,1 +1,20 @@
+class Solution {
+    isPrime(n) {
+        if (n <= 1) {
+            return false;
+        }
+        if (n === 2) {
+            return true;
+        }
+        if (n % 2 === 0) {
+            return false;
+        }
+        for (let i = 3; i <= Math.sqrt(n); i += 2) {
+            if (n % i === 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
 
